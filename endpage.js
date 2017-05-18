c9c60487049fd08a49b0a8deb6f22a0abb2766f1
Tina Vrieler <tinavrieler@gmail.com>
2017-05-18 13:36:55 +0200

    no message

--- endpage.js
+++ endpage.js
@@ -9,7 +9,7 @@ ctx.strokeStyle = ctx.fillStyle = "#ff0066";
 
 (function loop() {
     ctx.clearRect(x, 0, 60, 150);
-    ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]); // create a long dash mask
+    ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]); // create a long dash mask to give illusion of drawing
     dashOffset -= speed;                                         // reduce dash length
     ctx.strokeText(txt[i], x, 90);                               // stroke letter
 
@@ -17,9 +17,11 @@ ctx.strokeStyle = ctx.fillStyle = "#ff0066";
     else {
         ctx.fillText(txt[i], x, 90);                               // fill final letter
         dashOffset = dashLen;                                      // prep next char
-        x += ctx.measureText(txt[i++]).width + ctx.lineWidth * Math.random();
+        x += ctx.measureText(txt[i++]).width + ctx.lineWidth * Math.random(); //random letter spacing
         ctx.setTransform(1, 0, 0, 1, 0, 30 * Math.random());        // random y-delta
-        ctx.rotate(Math.random() * 0.05);                         // random rotation
+        ctx.rotate(Math.random() * 0.05);                         // random subtle rotation
         if (i < txt.length) requestAnimationFrame(loop);
     }
 })();
+
+// Code adapted from: http://stackoverflow.com/questions/29911143/how-can-i-animate-the-drawing-of-text-on-a-web-page
--- i18n.js
+++ i18n.js
@@ -13,11 +13,11 @@ var json;
 //String lookup tables in json format. Separate key/value pairs with a comma
 var english = '{"startText": "Start!","puzzleHelp": "Click on any two puzzle pieces to swap their places","puzzleHeader": "Puzzle Game","dog":"Dog",' +
     '"cat":"Cat","pig":"Guinea Pig","horse":"Horse","elephant":"Elephant","donkey":"Donkey","scratchHelp":"Press mouse and move over to reveal a hidden image",' +
-    '"scratchHeader":"Scratch Off Game","scribbleHeader":"Scribble game","scribbleHelp":"Guess the appearing image as fast as possible!"}'
+    '"scratchHeader":"Scratch Off Game","scribbleHeader":"Scribble game","scribbleHelp":"Guess the appearing image as fast as possible!"}';
 
-var swedish = '{"startText": "Börja!","puzzleHelp": "Clicka på två puzzelbitar för att få de att byta plats med varandra", "puzzleHeader": "Puzzle Game","dog":"Hund",' +
+var swedish = '{"startText": "Börja!","puzzleHelp": "Clicka på två puzzelbitar för att få de att byta plats med varandra", "puzzleHeader": "Puzzle Spel","dog":"Hund",' +
     '"cat":"Katt","pig":"Marsvin","horse":"Häst","elephant":"Elefant","donkey":"åsna","scratchHelp":"Press mouse and move over to reveal a hidden Image",' +
-    '"scratchHeader":"Scratch Off Game","scribbleHeader":"Scribble game","scribbleHelp":"Guess the appearing image as fast as possible!"}'
+    '"scratchHeader":"Scratch Off Game","scribbleHeader":"Scribble game","scribbleHelp":"Guess the appearing image as fast as possible!"}';
 
 
 function changeLanguage() {
