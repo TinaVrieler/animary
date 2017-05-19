ce8fafa2096e58b0569745e3d3d9ae864340c81c
Mika <meiki.leja@gmail.com>
2017-05-19 21:27:25 +0200

    voice

---
+++ ButtonSounds.js
@@ -0,0 +1,39 @@
+/**
+ * Created by mika on 5/19/17.
+ */
+
+var dog = $(".dogVoice")[0];
+
+$(".dogButton").mouseenter(function() {
+    dog.play();
+});
+
+var cat = $(".catVoice")[0];
+
+$(".catButton").mouseenter(function() {
+    cat.play();
+});
+
+var elephant = $(".elephantVoice")[0];
+
+$(".elephantButton").mouseenter(function() {
+    elephant.play();
+});
+
+var pig = $(".pigVoice")[0];
+
+$(".pigButton").mouseenter(function() {
+    pig.play();
+});
+
+var donkey = $(".donkeyVoice")[0];
+
+$(".donkeyButton").mouseenter(function() {
+    donkey.play();
+});
+
+var horse = $(".horseVoice")[0];
+
+$(".horseButton").mouseenter(function() {
+    horse.play();
+});
